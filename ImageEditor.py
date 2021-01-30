af3f1bbe601046d5151179ca990d4cc99abd18a5
Ryan <ryanporter8@hotmail.com>
2021-01-29 22:51:36 -0500

    Added red image functionality

--- ImageEditor.py
+++ ImageEditor.py
@@ -6,9 +6,9 @@ from PIL import Image
 import numpy as np
 
 img = Image.open("ct.jpg")
-
-img_chn_red = np.zeros(img.shape,dtype='uint8')
-img_chn_red[:,:,0] = img[:,:,0]
+img_data = np.array(img)
+img_chn_red = np.zeros(img_data.shape,dtype='uint8')
+img_chn_red[:,:,0] = img_data[:,:,0]
 img_red = Image.fromarray(img_chn_red)
 img_red.show()
 

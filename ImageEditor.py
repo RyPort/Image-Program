c671597204e742f3110b69c0253959bb9652793d
Ryan <ryanporter8@hotmail.com>
2021-01-29 22:21:54 -0500

    Second Commit

--- ImageEditor.py
+++ ImageEditor.py
@@ -2,4 +2,14 @@
 # Ryan Porter
 # Agusto Cohn
 # Richard Bailey
+from PIL import Image
+import numpy as np
+
+img = Image.open("ct.jpg")
+
+img_chn_red = np.zeros(img.shape,dtype='uint8')
+img_chn_red[:,:,0] = img[:,:,0]
+img_red = Image.fromarray(img_chn_red)
+img_red.show()
+
 

00c262959598d5b3984c1c3ada481ae3017cf212
Ryan <ryanporter8@hotmail.com>
2021-01-30 13:42:00 -0500

    made image turn all black

--- ImageEditor.py
+++ ImageEditor.py
@@ -6,17 +6,20 @@ from PIL import Image
 import numpy as np
 
 img = Image.open("ct.jpg")
-img_data = np.array(img)
-img_chn_red = np.zeros(img_data.shape,dtype='uint8')
-img_chn_red[:,:,0] = img_data[:,:,0]
-img_red = Image.fromarray(img_chn_red)
-img_red.show()
+
+# create an array from the image
+
+img_ar = np.array(Image.open("ct.jpg"))
+
+for row in img_ar:
+    for pixel in row:
+        pixel[0] = 0
+        pixel[1] = 0
+        pixel[2] = 0
+
+img_2 = Image.fromarray(img_ar)
 
 
-print("Hello World")
 
-#richies music is really loud
 
-#testing login credentials
-
-#testing cedentials
+img_2.show()
